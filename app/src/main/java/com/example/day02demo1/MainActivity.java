2393fb7856cbb95c335586d26bfd733e229c2d31
yijiannijuxiao <1782946883@qq.com>
2017-09-12 10:44:42 +0800

    第二次提交

--- app/src/main/java/com/example/day02demo1/MainActivity.java
+++ app/src/main/java/com/example/day02demo1/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.day02demo1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout activity_main;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        Log.e("qwertyu","-----------");
+    }
+
+    private void initView() {
+        activity_main = (RelativeLayout) findViewById(R.id.activity_main);
+        tv1 = (TextView) findViewById(R.id.tv1);
     }
 }
